54fda80752baa848f9c8f5d7064227976bada0a8
Wen-Lin Lee <82gogolee@gmail.com>
2022-07-27 14:08:20 +0800

    apply styles

--- src/App.js
+++ src/App.js
@@ -1,46 +1,68 @@
-import './App.css';
-
 import React, { useState } from 'react';
 function App() {
   //states
   const [newItem, setNewItem] = useState('');
   const [items, setItems] = useState([]);
+  const [error, setError] = useState('');
 
   //helper functions
   const addItem = () => {
     if (!newItem) {
-      alert('Enter an item');
+      setError('Please enter an item!');
       return;
     }
+    setError('');
     const item = {
       id: Math.floor(Math.random() * 1000),
       value: newItem,
     };
     setItems(oldList => [...oldList, item]);
     setNewItem('');
-    console.log(items);
   };
   const deleteItem = id => {
-    console.log('deleted');
     const newArray = items.filter(item => item.id !== id);
     setItems(newArray);
   };
+
   return (
-    <div className="App">
-      <h1>Todo List App</h1>
+    <div className="flex justify-center items-center flex-col min-h-full p-3">
+      <h1 className="text-3xl p-2">Todo List App</h1>
       <input
+        className="p-2 outline m-2 rounded-lg shadow-md w-80 text-green-500"
         type="text"
         placeholder="add an item..."
         onChange={e => setNewItem(e.target.value)}
         value={newItem}
       />
-      <button onClick={() => addItem()}>Add</button>
+      <button className="p-2" onClick={() => addItem()}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+        >
+          <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-5v5h-2v-5h-5v-2h5v-5h2v5h5v2z" />
+        </svg>
+      </button>
+      <p className="text-red-500 font-bold p-2">{error}</p>
       <ul>
         {items.map(item => {
           return (
-            <li key={item.id}>
-              {item.value}{' '}
-              <button onClick={() => deleteItem(item.id)}>X</button>
+            <li
+              key={item.id}
+              className="text-2xl flex items-center text-green-500 font-bold p-2"
+            >
+              {item.value}
+              <button className="pl-2" onClick={() => deleteItem(item.id)}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  className="fill-orange-500 "
+                >
+                  <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z" />
+                </svg>
+              </button>
             </li>
           );
         })}
