307ed10ea5f5c66895c9c8d4643e9330c8224d16
Wen-Lin Lee <82gogolee@gmail.com>
2022-07-27 14:53:10 +0800

    adjust styles

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   const [newItem, setNewItem] = useState('');
   const [items, setItems] = useState([]);
   const [error, setError] = useState('');
+  const [updateItem, setUpdateItem] = useState('');
 
   //helper functions
   const addItem = () => {
@@ -26,9 +27,9 @@ function App() {
 
   return (
     <div className="flex justify-center items-center flex-col min-h-full p-3">
-      <h1 className="text-3xl p-2">Todo List App</h1>
+      <h1 className="text-3xl p-2 text-orange-500 font-semibold">Todo List</h1>
       <input
-        className="p-2 outline m-2 rounded-lg shadow-md w-80 text-green-500"
+        className="p-2 outline m-2 rounded-lg shadow-md w-60 text-green-500"
         type="text"
         placeholder="add an item..."
         onChange={e => setNewItem(e.target.value)}
@@ -40,11 +41,12 @@ function App() {
           width="24"
           height="24"
           viewBox="0 0 24 24"
+          className="fill-orange-300"
         >
           <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-5v5h-2v-5h-5v-2h5v-5h2v5h5v2z" />
         </svg>
       </button>
-      <p className="text-red-500 font-bold p-2">{error}</p>
+      <p className="text-orange-700 font-bold p-2">{error}</p>
       <ul>
         {items.map(item => {
           return (
